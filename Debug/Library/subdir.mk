18b785526a315d5a0f0f58a40b940beab89da857
Kristian HARGE <kristian_harge@hotmail.com>
2022-02-17 19:33:25 +0100

    First commit, code done

---
+++ Debug/Library/subdir.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (9-2020-q2-update)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Library/GPIO_Interrupt_callback_dispatch.c 
+
+OBJS += \
+./Library/GPIO_Interrupt_callback_dispatch.o 
+
+C_DEPS += \
+./Library/GPIO_Interrupt_callback_dispatch.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Library/%.o: ../Library/%.c Library/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m0 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F030x6 -c -I../Core/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc -I../Drivers/STM32F0xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F0xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
